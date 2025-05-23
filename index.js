e236ddb25b9edfc83443f63303b1913f121aa8ed
Rahul Sagar <fjtelent7726@gmail.com>
2025-05-23 17:42:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+// keyword let , var and const use
+  
+ 
+var rahul =12;
+rahul=23;
+ console.log(rahul)
+ console.log(rahul);  // var me resend value and rediclear value kr sakte hai 
+ 
+
+
+ let raju=21;
+  raju=20;
+ console.log(raju);
+ console.log(raju);
+ //let me  resend value kr sakte and rediclear nhi kr sakte
+ 
+ 
+ const shivam =30;
+ console.log(shivam);  // const  me  resend value and rediclear nhi ker sakte
+
+
+
+
+ 
+ 
